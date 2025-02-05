512a9d3453e1745447179c6cb7aee0985e6c8aa1
notBigO <2003varunp@gmail.com>
2025-02-05 16:24:21 +0530

    refactor: move Todo functionality to separate module and service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,37 +12,4 @@ import { ApiBody } from '@nestjs/swagger';
 import { CreateTodoDto, UpdateTodoDto } from './todo.dto';
 
 @Controller('todos')
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Post()
-  @ApiBody({ type: CreateTodoDto })
-  createTodo(@Body('title') title: string) {
-    return this.appService.createTodo(title);
-  }
-
-  @Get()
-  getTodos() {
-    return this.appService.getTodos();
-  }
-
-  @Get(':id')
-  getTodoById(@Param('id') id: number) {
-    return this.appService.getTodoById(Number(id));
-  }
-
-  @Put(':id')
-  @ApiBody({ type: UpdateTodoDto })
-  updateTodo(@Param('id') id: number, @Body() updateTodoDto: UpdateTodoDto) {
-    return this.appService.updateTodo(
-      Number(id),
-      updateTodoDto.title,
-      updateTodoDto.completed,
-    );
-  }
-
-  @Delete(':id')
-  deleteTodo(@Param('id') id: number) {
-    return this.appService.deleteTodo(Number(id));
-  }
-}
+export class AppController {}
---
+++ src/todo/todo.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { ApiBody } from '@nestjs/swagger';
+import { CreateTodoDto, UpdateTodoDto } from 'src/todo.dto';
+
+@Controller('todo')
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Post()
+  @ApiBody({ type: CreateTodoDto })
+  createTodo(@Body('title') title: string) {
+    return this.todoService.createTodo(title);
+  }
+
+  @Get()
+  getTodos() {
+    return this.todoService.getTodos();
+  }
+
+  @Get(':id')
+  getTodoById(@Param('id') id: number) {
+    return this.todoService.getTodoById(Number(id));
+  }
+
+  @Put(':id')
+  @ApiBody({ type: UpdateTodoDto })
+  updateTodo(@Param('id') id: number, @Body() updateTodoDto: UpdateTodoDto) {
+    return this.todoService.updateTodo(
+      Number(id),
+      updateTodoDto.title,
+      updateTodoDto.completed,
+    );
+  }
+
+  @Delete(':id')
+  deleteTodo(@Param('id') id: number) {
+    return this.todoService.deleteTodo(Number(id));
+  }
+}
